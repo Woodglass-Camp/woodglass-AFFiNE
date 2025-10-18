626f92c34ec02a9abb65ee62181adc0cb906de1c
emasi <ad56583964@outlook.com>
2025-10-18 17:52:27 +0800

    fix: reuse edgeless toolbar for gridmap

--- blocksuite/affine/blocks/root/src/gridmap/gridmap-toolbar-extension.ts
+++ blocksuite/affine/blocks/root/src/gridmap/gridmap-toolbar-extension.ts
@@ -2,41 +2,26 @@ import {
   type ViewExtensionContext,
   ViewExtensionProvider,
 } from '@blocksuite/affine-ext-loader';
-import { EdgelessToolbarWidget } from '@blocksuite/affine-widget-edgeless-toolbar';
-import { WidgetViewExtension } from '@blocksuite/std';
-import { literal, unsafeStatic } from 'lit/static-html.js';
+import { effects } from '@blocksuite/affine-widget-edgeless-toolbar/effects';
+import { edgelessToolbarWidget } from '@blocksuite/affine-widget-edgeless-toolbar';
 
-export const GRIDMAP_TOOLBAR_TAG = 'gridmap-toolbar-widget';
 export const GRIDMAP_NOTE_CHILD_FLAVOUR = 'affine:paragraph';
 export const GRIDMAP_NOTE_CHILD_TYPE = 'text';
 export const GRIDMAP_NOTE_TIP = 'Text';
 
-function defineGridmapToolbarElement() {
-  if (customElements.get(GRIDMAP_TOOLBAR_TAG)) {
-    return;
-  }
-  class GridmapToolbarWidget extends EdgelessToolbarWidget {}
-  customElements.define(GRIDMAP_TOOLBAR_TAG, GridmapToolbarWidget);
-}
-
-const gridmapToolbarWidget = WidgetViewExtension(
-  'affine:page',
-  GRIDMAP_TOOLBAR_TAG,
-  literal`${unsafeStatic(GRIDMAP_TOOLBAR_TAG)}`
-);
-
 export class GridmapToolbarViewExtension extends ViewExtensionProvider {
   override name = 'affine-gridmap-toolbar-widget';
 
-  override effect() {
+  override effect(): void {
     super.effect();
-    defineGridmapToolbarElement();
+    effects();
   }
 
   override setup(context: ViewExtensionContext) {
     super.setup(context);
     if (context.scope === 'gridmap') {
-      context.register(gridmapToolbarWidget);
+      context.register(edgelessToolbarWidget);
     }
   }
 }
+
